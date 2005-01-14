3982a3fc3c5e2485bce3b0ba0366793739fb66e0
Peter De Rijk <Peter.DeRijk@ua.ac.be>
2005-01-14 17:54:31 +0000

    extension support

---
+++ init.tcl
@@ -0,0 +1,11 @@
+# This file provides for an alternative loading of extensions
+# based on directory.
+# in order to load the given package, this file is sourced
+# When this script is sourced, the variable $dir must contain the
+# full path name of the xtensions directory.
+
+namespace eval ::interface {}
+set ::interface::dir $dir
+source [file join $dir lib interface.tcl]
+lappend auto_path [file join $dir lib]
+extension provide interface 0.8.9
